d65a4a1a9ed128fcd4f68ab753634f434495c8d1
Michael Maloney <maloney.mc@gmail.com>
2010-07-14 12:59:22 -0400

    adding RSS routing/controller stuff

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,6 +1,9 @@
 class TracksController < ApplicationController
   def index
-    @tracks = Track.all
+    respond_to do |format|
+      format.html { @tracks = Track.all }
+      format.rss { render :xml => Track.rss }
+    end
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :tracks
   
+  map.formatted_tracks 'tracks.:format', :controller => 'tracks', :action => 'index'
+  
   map.root :controller => 'tracks'
 
   map.connect ':controller/:action/:id'
