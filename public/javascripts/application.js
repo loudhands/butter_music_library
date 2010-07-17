0996c9319139aa2453cc4be39dc446e97805d449
Michael Maloney <maloney.mc@gmail.com>
2010-07-17 18:27:08 -0400

    javascript for multiple select

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+	toggleButtons();
+});
+
+function toggleButtons() {
+	if ($("#tracks_container :input").is(":checked")) {
+		$("#group_edit_button").removeAttr('disabled');
+		$("#group_delete_button").removeAttr('disabled');
+	} else {
+		$("#group_edit_button").attr('disabled', true);
+		$("#group_delete_button").attr('disabled', true);
+	}
+}
