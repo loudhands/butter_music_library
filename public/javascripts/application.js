f79b60334c0b7c3058c3bffd32f555cbc16f40e6
robertjwhitney <robertj.whitney@gmail.com>
2010-07-19 19:57:08 -0400

    toggle for disable on bottom buttons

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	toggleButtons();
 	selectAll();
-	$("table#tracks_container").tablesorter(); 
+	$("table#tracks_container :not('input')").tablesorter(); 
 });
 
 function toggleButtons() {
-	if ($("#tracks_container :input").is(":checked")) {
+	if ($("#tracks_container :input").is(":checked"))  {
 		$("#group_edit_button").removeAttr('disabled');
 		$("#group_delete_button").removeAttr('disabled');
 	} else {
@@ -15,9 +15,13 @@ function toggleButtons() {
 }
 
 function selectAll() {
-  var tog = false; // or true if they are checked on load 
+  var tog = false; // or true if they are checked on load
+  var able = true;
    $('#tracks_container th :input').click(function() { 
-      $("input[type=checkbox]").attr("checked",!tog); 
-    tog = !tog; 
+      $("input[type=checkbox]").attr("checked",!tog);
+      $("#group_edit_button").attr("disabled",!able);
+      $("#group_delete_button").attr("disabled",!able);
+    tog = !tog;
+    able = !able; 
    });
 }
